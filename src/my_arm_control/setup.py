ecb4b6b8cc15e96d0d66af14a33963f8f8116d16
Dr. Manel Puig i Vidal <36498743+manelpuig@users.noreply.github.com>
2026-01-07 17:56:45 +0100

    joint traj2

--- src/my_arm_control/setup.py
+++ src/my_arm_control/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from glob import glob
+import os
 
 package_name = "my_arm_control"
 
@@ -7,16 +9,25 @@ setup(
     version="0.0.1",
     packages=[package_name],
     data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        # Required for ROS 2 package discovery
+        ("share/ament_index/resource_index/packages",
+         ["resource/" + package_name]),
+
         ("share/" + package_name, ["package.xml"]),
-        ("share/" + package_name + "/launch", ["launch/*.py"]),
-        ("share/" + package_name + "/config", ["config/*.yaml"]),
+
+        # Launch files
+        (os.path.join("share", package_name, "launch"),
+         glob("launch/*.launch.py")),
+
+        # YAML config files
+        (os.path.join("share", package_name, "config"),
+         glob("config/*.yaml")),
     ],
     install_requires=["setuptools", "pyserial"],
     zip_safe=True,
     maintainer="you",
     maintainer_email="you@todo.todo",
-    description="Simple ROS2 control nodes for a 6DOF servo arm over USB serial.",
+    description="ROS 2 control nodes for a 6-DOF arm (trajectory, serial, estimation).",
     license="Apache-2.0",
     entry_points={
         "console_scripts": [
