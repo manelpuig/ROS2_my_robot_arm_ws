2dc745ceeeaea5c39d06716da3b12afabc351f23
Dr. Manel Puig i Vidal <36498743+manelpuig@users.noreply.github.com>
2026-01-10 09:06:45 +0100

    xacro

--- src/my_arm_description/launch/display.launch.py
+++ src/my_arm_description/launch/display.launch.py
@@ -4,7 +4,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, FindExecutable
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
@@ -30,7 +30,7 @@ def generate_launch_description():
     xacro_file = PathJoinSubstitution([pkg_share, "urdf", arm_file])
 
     robot_description = ParameterValue(
-        Command(["xacro", xacro_file]),
+        Command([FindExecutable(name="xacro"), " ", xacro_file]),
         value_type=str
     )
 
